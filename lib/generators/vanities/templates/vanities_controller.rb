b7b14a15051df71138b61e6ce32b1c5f936c19dd
J. Austin Hughey <jaustinhughey@gmail.com>
2011-03-20 07:02:03 -0600

    Slight refactor to make the vanities controller render a 404 if it can't find a vanity object.  Small documentation update about routes (make sure vanities is the LAST route in your routes.rb file).

--- lib/generators/vanities/templates/vanities_controller.rb
+++ lib/generators/vanities/templates/vanities_controller.rb
@@ -1,8 +1,18 @@
 class VanitiesController < ApplicationController
+
   def show
-    v = Vanity.find_by_name params[:vname]
-    redirect_to v.vain
     # Note that in this case, any model using the vanities system
     # MUST be set up via REST.
+
+    if(v = Vanity.find_by_name(params[:vname]))
+      redirect_to v.vain
+    else
+      # If the vanity name can't be found, render a 404
+      respond_to do |f|
+        f.html { render :status => :not_found, :file => File.join(Rails.root, "public", "404.html") }
+        f.xml  { head :not_found }
+        f.any  { head :not_found }
+      end
+    end    
   end
 end
